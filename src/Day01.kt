da97ee13bd4ed86d320fb04981238a17604996a5
Thomas Baker <thomaslbaker@outlook.com>
2022-12-01 14:46:37 -0500

    Day 1 complete

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,15 +1,40 @@
 fun main() {
-    fun part1(input: List<String>): Int {
-        return input.size
+    fun part1(input: List<String>): Long {
+        var prevCalCount: Long = 0
+        val lastCalCount = input.fold(0L) { acc, cal ->
+            val cur = cal.toLongOrNull()
+            if (cur == null) {
+                if (prevCalCount < acc) {
+                    prevCalCount = acc
+                }
+                0
+            } else {
+                acc + cur
+            }
+        }
+        return maxOf(prevCalCount, lastCalCount)
     }
 
-    fun part2(input: List<String>): Int {
-        return input.size
+    fun part2(input: List<String>): Long {
+        val elfCounts = mutableListOf<Long>()
+        elfCounts.add(
+            input.fold(0L) { acc, cal ->
+                val cur = cal.toLongOrNull()
+                if (cur == null) {
+                    elfCounts.add(acc)
+                    0
+                } else {
+                    acc + cur
+                }
+            }
+        )
+        return elfCounts.sortedDescending().toList().take(3).sum()
     }
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    check(part1(testInput) == 24000L)
+    check(part2(testInput) == 45000L)
 
     val input = readInput("Day01")
     println(part1(input))
