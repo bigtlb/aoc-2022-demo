86f717cdf3039c281643548d1b5834f0ed30abe0
Thomas Baker <thomaslbaker@outlook.com>
2022-12-04 11:22:29 -0500

    Day 4 complete

---
+++ src/Day04.kt
@@ -0,0 +1,30 @@
+fun main() {
+
+    fun List<String>.parseList(): List<Pair<IntRange, IntRange>> = this.map {
+        it.split(',').zipWithNext { a, b ->
+            Pair(
+                a.split('-').map(String::toInt).zipWithNext { begin, end -> IntRange(begin, end) }.single(),
+                b.split('-').map(String::toInt).zipWithNext { begin, end -> IntRange(begin, end) }.single()
+            )
+        }.single()
+    }
+
+    fun part1(input: List<String>): Int = input.parseList()
+        .sumOf { if ((it.first subtract it.second.toSet()).isNotEmpty() && (it.second subtract it.first.toSet()).isNotEmpty()) 0 else 1 as Int }
+
+
+
+    fun part2(input: List<String>): Int = input.parseList()
+        .sumOf { if ((it.first intersect it.second.toSet()).isNotEmpty()) 1 else 0 as Int }
+
+
+    // test if implementation meets criteria from the description, like:
+    val testInput = readInput("Day04_test")
+    val part1 = part1(testInput)
+    check(part1 == 2)
+    check(part2(testInput) == 4)
+
+    val input = readInput("Day04")
+    println(part1(input))
+    println(part2(input))
+}
